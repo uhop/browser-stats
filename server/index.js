e841d71a7c8839096fbebcc0b01341603204c712
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2023-05-11 09:32:05 -0500

    Implement DOM manipulations in terms of h.js.

--- server/index.js
+++ server/index.js
@@ -1,30 +1,131 @@
 import {formatInteger, formatNumber} from './formatters.js';
-
-const h = (tag = 'span') => document.createElement(tag);
+import {build} from './h.js';
 
 const makeHeader = (root, data) => {
-  const totalUnknownBrowsers = Object.keys(data.stats.unknownBrowsers).length,
-    section = h('section'),
-    p1 = h('p'),
-    p2 = h('p'),
-    p3 = h('p');
+  const totalUnknownBrowsers = Object.keys(data.stats.unknownBrowsers).length;
+  build(
+    [
+      'section',
+      [
+        'p',
+        {
+          $: {
+            innerHTML: `Statistics: found ${formatInteger(
+              data.stats.knownBrowsers + totalUnknownBrowsers
+            )} distinct browsers &mdash; ${formatInteger(data.stats.knownBrowsers)} known and ${formatInteger(
+              totalUnknownBrowsers
+            )} unknown browsers. After clustering we found ${formatInteger(data.stats.uniqueBrowsers)} unique browsers.`
+          }
+        }
+      ],
+      [
+        'p',
+        `The unknown browsers cover ${formatInteger(data.stats.unknownUsers)} users (${formatNumber(
+          (data.stats.unknownUsers / data.stats.totalUsers) * 100,
+          {decimals: 2}
+        )}%). Users of unknown browsers will be ignored.`
+      ],
+      ['p', `The data below cover ${formatInteger(data.stats.adjustedTotalUsers)} users with known browsers.`]
+    ],
+    root
+  );
+};
 
-  p1.innerHTML = `Statistics: found ${formatInteger(
-    data.stats.knownBrowsers + totalUnknownBrowsers
-  )} distinct browsers &mdash;
-    ${formatInteger(data.stats.knownBrowsers)} known and ${formatInteger(
-    totalUnknownBrowsers
-  )} unknown browsers. After clustering we found ${formatInteger(data.stats.uniqueBrowsers)} unique browsers.`;
+const makeInput = (type, name, value, labelText, active) => [
+  'label',
+  ['input', {type, name, value, $: {checked: !!active}}],
+  typeof labelText == 'string' ? ' ' + labelText : labelText
+];
 
-  p2.innerHTML = `The unknown browsers cover ${formatInteger(data.stats.unknownUsers)} users (${formatNumber(
-    (data.stats.unknownUsers / data.stats.totalUsers) * 100,
-    {decimals: 2}
-  )}%). Users of unknown browsers will be ignored.`;
+const makeRadioButton = (name, value, labelText, active) => makeInput('radio', name, value, labelText, active);
+const makeCheckbox = (name, value, labelText, active) => makeInput('checkbox', name, value, labelText, active);
 
-  p3.innerHTML = `The data below cover ${formatInteger(data.stats.adjustedTotalUsers)} users with known browsers.`;
+const formControl = event => {
+  const target = event.target;
+  if (target.tagName !== 'INPUT') return;
+  const roots = document.querySelectorAll('.feature-list');
+  if (!roots.length) return;
+  if (target.type === 'radio') {
+    if (target.name === 'frame') {
+      const oldFrames = Array.from(roots[0].classList).filter(name => /^frame\d+$/.test(name));
+      [...roots, ...document.querySelectorAll('.browser-list')].forEach(root => {
+        root.classList.remove(...oldFrames);
+        root.classList.add(target.value);
+      });
+      return;
+    }
+    if (target.name === 'visibility') {
+      switch (target.value) {
+        case 'available':
+          Array.from(roots).forEach(root => {
+            root.classList.remove('hide-available');
+            root.classList.add('hide-missing');
+          });
+          break;
+        case 'missing':
+          Array.from(roots).forEach(root => {
+            root.classList.remove('hide-missing');
+            root.classList.add('hide-available');
+          });
+          break;
+        default:
+          Array.from(roots).forEach(root => {
+            root.classList.remove('hide-available', 'hide-missing');
+          });
+          break;
+      }
+      return;
+    }
+  }
+  if (target.type === 'checkbox') {
+    if (target.name === 'table') {
+      Array.from(roots).forEach(root => {
+        const section = root.closest('section');
+        if (!section) return;
+        if (root.tagName === 'TABLE') {
+          section.style.display = target.checked ? 'block' : 'none';
+        } else {
+          section.style.display = target.checked ? 'none' : 'block';
+        }
+      });
+    }
+  }
+};
 
-  section.append(p1, p2, p3);
-  root.append(section);
+const makeControls = (root, data) => {
+  build(
+    [
+      'section',
+      [
+        'form.feature-control',
+        {submit: event => event.preventDefault(), change: formControl},
+        [
+          'fieldset',
+          ['legend', 'Percentage of users'],
+          ...data.frames.map((frame, index) =>
+            makeRadioButton(
+              'frame',
+              'frame' + index,
+              `${formatNumber((frame.users / data.stats.adjustedTotalUsers) * 100, {decimals: 2})}% (${formatInteger(
+                frame.users
+              )} users)`,
+              !index
+            )
+          )
+        ],
+        [
+          'fieldset',
+          ['legend', 'Show features'],
+          makeRadioButton('visibility', 'both', 'show all features', true),
+          makeRadioButton('visibility', 'available', 'available only'),
+          makeRadioButton('visibility', 'missing', 'missing only'),
+          ['span', {$: {innerHTML: '&nbsp;'}}],
+          makeCheckbox('table', 'yes', 'show as table')
+        ]
+      ]
+    ],
+    root
+  );
 };
 
 const listFrameClasses = (featureName, featureFrames) =>
@@ -50,46 +151,52 @@ const calcFeatureFrames = data => {
 
 const makeList = (root, data) => {
   const featureFrames = calcFeatureFrames(data);
-
-  const p = h('p');
-  p.className = 'feature-list frame0';
-  p.innerHTML = data.frames[0].available
-    .map(
-      featureName =>
-        `<span class="feature-item ${listFrameClasses(featureName, featureFrames).join(' ')}"><a href="${
-          'https://caniuse.com/' + featureName
-        }" title="${data.featureTitles[featureName] || ''}">${featureName}</a></span>`
-    )
-    .join(' ');
-
-  const section = h('section');
-  section.append(p);
-  root.append(section);
+  build(
+    [
+      'section',
+      [
+        'p.feature-list.frame0',
+        ...data.frames[0].available.map(featureName => [
+          `span.feature-item${listFrameClasses(featureName, featureFrames)
+            .map(name => '.' + name)
+            .join('')}`,
+          ['a', {href: 'https://caniuse.com/' + featureName, title: data.featureTitles[featureName] || ''}, featureName]
+        ])
+      ]
+    ],
+    root
+  );
 };
 
 const makeTable = (root, data) => {
   const featureFrames = calcFeatureFrames(data);
-
-  const table = h('table');
-  table.className = 'feature-list frame0';
-  table.innerHTML =
-    '<thead><tr><th>Feature name</th><th>Description</th></tr></thead><tbody>' +
-    data.frames[0].available
-      .map(
-        featureName =>
-          `<tr class="feature-item ${listFrameClasses(featureName, featureFrames).join(' ')}"><td><a href="${
-            'https://caniuse.com/' + featureName
-          }" title="${data.featureTitles[featureName] || ''}">${featureName}</a></td><td>${
-            data.featureTitles[featureName] || ''
-          }</td></tr>`
-      )
-      .join('') +
-    '</tbody>';
-
-  const section = h('section');
-  section.style.display = 'none';
-  section.append(table);
-  root.append(section);
+  build(
+    [
+      'section',
+      {style: {display: 'none'}},
+      [
+        'table.feature-list.frame0',
+        ['thead', ['tr', ['th', 'Feature name'], ['th', 'Description']]],
+        [
+          'tbody',
+          ...data.frames[0].available.map(featureName => [
+            'tr',
+            {className: ['feature-item', ...listFrameClasses(featureName, featureFrames)]},
+            [
+              'td',
+              [
+                'a',
+                {href: 'https://caniuse.com/' + featureName, title: data.featureTitles[featureName] || ''},
+                featureName
+              ]
+            ],
+            ['td', data.featureTitles[featureName] || '']
+          ])
+        ]
+      ]
+    ],
+    root
+  );
 };
 
 const listBrowserFrameClasses = (from, to) => {
@@ -111,234 +218,123 @@ export const stringify = version =>
   (version.buildMeta ? '+' + buildMeta : '');
 
 const makeBrowserTable = (root, data) => {
-  const featureFrames = calcFeatureFrames(data);
-
-  const table = h('table');
-  table.className = 'browser-list frame0';
-
   let currentBrowserIndex = 0;
-  table.innerHTML =
-    '<thead><tr><th class="right">#</th><th>Browser</th><th>Version</th><th class="right">Users</th></tr></thead><tbody>' +
-    data.frames
-      .map((frame, index) =>
-        frame.browsers
-          .map(
-            item =>
-              `<tr class="browser-item ${listBrowserFrameClasses(index, data.frames.length).join(
-                ' '
-              )}"><td class="right">${formatInteger(++currentBrowserIndex)}</td><td>${item.browser}</td><td>${stringify(
-                item.version
-              )}</td><td class="right">${formatInteger(item.users)}</td></tr>`
-          )
-          .join('')
-      )
-      .join('') +
-    '</tbody>';
-
-  const section = h('section'),
-    details = h('details'),
-    summary = h('summary');
-  summary.innerHTML = `The list of known browsers (${formatInteger(currentBrowserIndex)})`;
-  details.append(summary, table);
-  section.append(details);
-  root.append(section);
+  const table = [
+    'table.browser-list.frame0',
+    ['thead', ['tr', ['th.right', '#'], ['th', 'Browser'], ['th', 'Version'], ['th.right', 'Users']]],
+    [
+      'tbody',
+      ...data.frames
+        .map((frame, index) =>
+          frame.browsers.map(item => [
+            'tr',
+            {className: ['browser-item', ...listBrowserFrameClasses(index, data.frames.length)]},
+            ['td.right', formatInteger(++currentBrowserIndex)],
+            ['td', item.browser],
+            ['td', stringify(item.version)],
+            ['td.right', formatInteger(item.users)]
+          ])
+        )
+        .flat()
+    ]
+  ];
+  build(
+    ['section', ['details', ['summary', `The list of known browsers (${formatInteger(currentBrowserIndex)})`], table]],
+    root
+  );
 };
 
 const makeUnknownBrowserTable = (root, data) => {
-  const table = h('table');
-  table.className = 'unknown-browser-list';
-
   let currentBrowserIndex = 0;
-  table.innerHTML =
-    '<thead><tr><th class="right">#</th><th>Browser</th><th class="right">Users</th></tr></thead><tbody>' +
-    Object.keys(data.stats.unknownBrowsers)
-      .map(
-        name =>
-          `<tr class="unknown-browser-item"><td class="right">${formatInteger(
-            ++currentBrowserIndex
-          )}</td><td>${name}</td><td class="right">${formatInteger(data.stats.unknownBrowsers[name])}</td></tr>`
-      )
-      .join('') +
-    '</tbody>';
-
-  const section = h('section'),
-    details = h('details'),
-    summary = h('summary');
-  summary.innerHTML = `The list of unknown browsers (${formatInteger(currentBrowserIndex)})`;
-  details.append(summary, table);
-  section.append(details);
-  root.append(section);
-};
-
-const makeInput = (type, name, value, labelText, active) => {
-  const input = h('input');
-  input.type = type;
-  input.name = name;
-  input.value = value;
-  input.checked = !!active;
-
-  const label = h('label');
-  label.append(input);
-  if (typeof labelText == 'string') {
-    label.append(document.createTextNode(' ' + labelText));
-  } else if (labelText) {
-    label.append(labelText);
-  }
-  return label;
-};
-
-const makeRadioButton = (name, value, labelText, active) => makeInput('radio', name, value, labelText, active);
-const makeCheckbox = (name, value, labelText, active) => makeInput('checkbox', name, value, labelText, active);
-
-const makeControls = (root, data) => {
-  const form = h('form'),
-    f1 = h('fieldset'),
-    f2 = h('fieldset');
-  form.append(f1, f2);
-  form.className = 'feature-control';
-
-  const l1 = h('legend');
-  l1.innerHTML = 'Percentage of users';
-  f1.append(l1);
-
-  const switchFrames = data.frames.map((frame, index) =>
-    makeRadioButton(
-      'frame',
-      'frame' + index,
-      `${formatNumber((frame.users / data.stats.adjustedTotalUsers) * 100, {decimals: 2})}% (${formatInteger(
-        frame.users
-      )} users)`,
-      !index
-    )
+  const table = [
+    'table.unknown-browser-list',
+    ['thead', ['tr', ['th.right', '#'], ['th', 'Browser'], ['th.right', 'Users']]],
+    [
+      'tbody',
+      ...Object.keys(data.stats.unknownBrowsers).map(name => [
+        'tr.unknown-browser-item',
+        ['td.right', formatInteger(++currentBrowserIndex)],
+        ['td', name],
+        ['td.right', formatInteger(data.stats.unknownBrowsers[name])]
+      ])
+    ]
+  ];
+  build(
+    [
+      'section',
+      ['details', ['summary', `The list of unknown browsers (${formatInteger(currentBrowserIndex)})`], table]
+    ],
+    root
   );
-  f1.append(...switchFrames);
-
-  const l2 = h('legend');
-  l2.innerHTML = 'Show features';
-  f2.append(l2);
-
-  const blankLine = h();
-  blankLine.innerHTML = '&nbsp;';
-
-  f2.append(
-    makeRadioButton('visibility', 'both', 'show all features', true),
-    makeRadioButton('visibility', 'available', 'available only'),
-    makeRadioButton('visibility', 'missing', 'missing only'),
-    blankLine,
-    makeCheckbox('table', 'yes', 'show as table')
-  );
-
-  form.addEventListener('change', event => {
-    const target = event.target;
-    if (target.tagName !== 'INPUT') return;
-    const roots = document.querySelectorAll('.feature-list');
-    if (!roots.length) return;
-    if (target.type === 'radio') {
-      if (target.name === 'frame') {
-        const oldFrames = Array.from(roots[0].classList).filter(name => /^frame\d+$/.test(name));
-        [...roots, ...document.querySelectorAll('.browser-list')].forEach(root => {
-          root.classList.remove(...oldFrames);
-          root.classList.add(target.value);
-        });
-        return;
-      }
-      if (target.name === 'visibility') {
-        switch (target.value) {
-          case 'available':
-            Array.from(roots).forEach(root => {
-              root.classList.remove('hide-available');
-              root.classList.add('hide-missing');
-            });
-            break;
-          case 'missing':
-            Array.from(roots).forEach(root => {
-              root.classList.remove('hide-missing');
-              root.classList.add('hide-available');
-            });
-            break;
-          default:
-            Array.from(roots).forEach(root => {
-              root.classList.remove('hide-available', 'hide-missing');
-            });
-            break;
-        }
-        return;
-      }
-    }
-    if (target.type === 'checkbox') {
-      if (target.name === 'table') {
-        Array.from(roots).forEach(root => {
-          const section = root.closest('section');
-          if (!section) return;
-          if (root.tagName === 'TABLE') {
-            section.style.display = target.checked ? 'block' : 'none';
-          } else {
-            section.style.display = target.checked ? 'none' : 'block';
-          }
-        });
-      }
-    }
-  });
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-  });
-
-  const section = h('section');
-  section.append(form);
-  root.append(section);
 };
 
 const makeSelectedFeatures = (root, data) => {
   if (!data.features) return;
 
-  const section = h('section');
+  let currentBrowserIndex = 0;
+  const table = unsupported => [
+    'table.unknown-browser-list',
+    ['thead', ['tr', ['th.right', '#'], ['th', 'Browser'], ['th.right', 'Users']]],
+    [
+      'tbody',
+      ...Object.entries(unsupported).map(([name, value]) => [
+        'tr.unknown-browser-item',
+        ['td.right', formatInteger(++currentBrowserIndex)],
+        ['td', name],
+        ['td.right', formatInteger(value)]
+      ])
+    ]
+  ];
 
-  for (const featureName of Object.keys(data.features)) {
-    const {users, unsupported} = data.features[featureName],
-      keys = Object.keys(unsupported),
-      h3 = h('h3'),
-      p = h('p'),
-      details = h('details'),
-      summary = h('summary'),
-      table = h('table');
-    h3.innerHTML = `<a href="https://caniuse.com/${featureName}" title="${data.featureTitles[featureName]}">${featureName}</a>: ${data.featureTitles[featureName]}`;
-    p.innerHTML = `This feature is supported by ${formatInteger(users)} users (${formatNumber(
-      (users / data.stats.adjustedTotalUsers) * 100,
-      {decimals: 2}
-    )}%).`;
-    table.className = 'unknown-browser-list';
-
-    let currentBrowserIndex = 0;
-    table.innerHTML =
-      '<thead><tr><th class="right">#</th><th>Browser</th><th class="right">Users</th></tr></thead><tbody>' +
-      keys
-        .map(
-          name =>
-            `<tr class="unknown-browser-item"><td class="right">${formatInteger(
-              ++currentBrowserIndex
-            )}</td><td>${name}</td><td class="right">${formatInteger(unsupported[name])}</td></tr>`
-        )
-        .join('') +
-      '</tbody>';
-
-    summary.innerHTML = `The list of browsers that do not support this feature (${formatInteger(keys.length)})`;
-    details.append(summary, table);
-    section.append(h3, p, details);
-  }
-
-  root.append(section);
+  build(
+    [
+      'section',
+      ...Object.entries(data.features)
+        .map(([featureName, {users, unsupported}]) => {
+          const keys = Object.keys(unsupported);
+          return [
+            [
+              'h3',
+              ['a', {href: 'https://caniuse.com/' + featureName, title: data.featureTitles[featureName]}, featureName],
+              ': ' + data.featureTitles[featureName]
+            ],
+            [
+              'p',
+              `This feature is supported by ${formatInteger(users)} users (${formatNumber(
+                (users / data.stats.adjustedTotalUsers) * 100,
+                {decimals: 2}
+              )}%).`
+            ],
+            [
+              'details',
+              ['summary', `The list of browsers that do not support this feature (${formatInteger(keys.length)})`],
+              table(unsupported)
+            ]
+          ];
+        })
+        .flat()
+    ],
+    root
+  );
 };
 
-const makeError = root => {
-  const p = h('p');
-  p.innerHTML =
-    'There is no <code>output.json</code>. ' +
-    'Please download the data file from Google Analytics and run <code>npm start</code> first.';
-
-  const section = h('section');
-  section.append(p);
-  root.append(section);
-};
+const makeError = root =>
+  build(
+    [
+      'section',
+      [
+        'p',
+        {
+          $: {
+            innerHTML:
+              'There is no <code>output.json</code>. ' +
+              'Please download the data file from Google Analytics and run <code>npm start</code> first.'
+          }
+        }
+      ]
+    ],
+    root
+  );
 
 const main = async () => {
   const root = document.getElementById('app'),
@@ -349,33 +345,17 @@ const main = async () => {
   makeHeader(root, data);
   makeControls(root, data);
 
-  {
-    const h2 = h('h2');
-    h2.innerHTML = 'Features';
-    root.append(h2);
-  }
+  build(['h2', 'Features'], root);
   makeList(root, data);
   makeTable(root, data);
 
-  {
-    const h2 = h('h2');
-    h2.innerHTML = 'Browsers';
-    root.append(h2);
-  }
+  build(['h2', 'Browsers'], root);
   makeBrowserTable(root, data);
 
-  {
-    const h2 = h('h2');
-    h2.innerHTML = 'Unknown browsers';
-    root.append(h2);
-  }
+  build(['h2', 'Unknown browsers'], root);
   makeUnknownBrowserTable(root, data);
 
-  {
-    const h2 = h('h2');
-    h2.innerHTML = 'Selected features';
-    root.append(h2);
-  }
+  build(['h2', 'Selected features'], root);
   makeSelectedFeatures(root, data);
 };
 
